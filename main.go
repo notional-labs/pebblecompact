b28a4ee5434155c2858d8f12f1188ead12bb674b
Tuan Pham Anh <baabeetaa@gmail.com>
2022-11-28 02:20:58 +0700

    goleveldb

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"github.com/cockroachdb/pebble"
+	"fmt"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/util"
 	"os"
 )
 
@@ -18,12 +20,13 @@ func main() {
 
 	dbPath := os.Args[1]
 
-	opts := &pebble.Options{
-		MaxOpenFiles: 100,
-	}
-	opts.EnsureDefaults()
+	//opts := &pebble.Options{
+	//	MaxOpenFiles: 100,
+	//}
+	//opts.EnsureDefaults()
 
-	db, err := pebble.Open(dbPath, opts)
+	//db, err := pebble.Open(dbPath, opts)
+	db, err := leveldb.OpenFile(dbPath, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -32,23 +35,26 @@ func main() {
 		db.Close()
 	}()
 
-	iter := db.NewIter(nil)
-	var start, end []byte
+	//iter := db.NewIter(nil)
+	//var start, end []byte
+	//
+	//if iter.First() {
+	//	start = cp(iter.Key())
+	//}
+	//
+	//if iter.Last() {
+	//	end = cp(iter.Key())
+	//}
+	//
+	//if err := iter.Close(); err != nil {
+	//	panic(err)
+	//}
 
-	if iter.First() {
-		start = cp(iter.Key())
-	}
-
-	if iter.Last() {
-		end = cp(iter.Key())
-	}
-
-	if err := iter.Close(); err != nil {
-		panic(err)
-	}
-
-	err = db.Compact(start, end, false)
+	//err = db.Compact(start, end, false)
+	err = db.CompactRange(util.Range{Start: nil, Limit: nil})
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("Done!")
 }
